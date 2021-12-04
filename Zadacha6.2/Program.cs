316118b9576683022d0c83e00b37f535d60c1671
Миша Мишин <spawn.rapture-0n@icloud.com>
2021-12-04 13:20:30 +0300

    Добавьте файлы проекта.

---
+++ Zadacha6.2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение (слова разделяются пробелами, знаки препинания не используются).");
+            string startString = Console.ReadLine();
+            string tempString = startString.ToLower();
+            string[] stringArray = tempString.Split();
+            string finalString=String.Join("",stringArray);
+            bool f = false;
+            for (int i = 0; i < finalString.Length / 2; i++)
+                if (finalString[i] != finalString[finalString.Length - i - 1])
+                {
+                    Console.WriteLine("Введенное предложение не является палиндромом.");
+                    f = true;
+                    break;
+                }
+            if (!f) Console.WriteLine("Введенное предложение является палиндромом.");
+            Console.ReadKey();
+        }
+    }
+}
